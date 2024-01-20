6623364dd7f942ba46668487cad3e469c34db1cb
PhongNT <phongnt@kaopiz.com>
2024-01-20 17:00:28 +0700

    clear unneccesary routes and vue file

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+    <Head title="Social Media Website" />
+    <h1>Test</h1>
+</template>
+
+<style>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,18 +16,17 @@ use Inertia\Inertia;
 |
 */
 
+Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('home');
+
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Home', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->middleware(['auth', 'verified']);
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
